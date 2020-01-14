22206ce45803fad1250d8ecd9a1ae753c76c102e
Nathan Faubion <nathan@awakesecurity.com>
2020-01-14 13:40:49 -0800

    Fix typo in FFI

--- src/Web/Promise/Rejection.js
+++ src/Web/Promise/Rejection.js
@@ -2,7 +2,7 @@ exports.fromError = function(a) {
   return a;
 };
 
-exports._toError = function(just, nothing, rej) {
+exports._toError = function(just, nothing, ref) {
   if (rej instanceof Error) {
     return just(ref);
   }
